757f2fd899f88b59985d49107a9b506891a0b2cc
dekampus <moiseswilliam.cs@gmail.com>
2024-07-29 17:10:50 -0300

    feat: Salva os dados de cadastro no sessionStorage

--- js/register-page.js
+++ js/register-page.js
@@ -7,4 +7,21 @@ registerForm.addEventListener('submit', (e) => {
     let cpf = document.getElementById('cpf').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+
+    saveToStorage(name, cpf, email, password);
 })
+
+function saveToStorage(name, cpf, email, password) {
+    let user = {
+        name: name,
+        cpf: cpf,
+        email: email,
+        password: password
+    };
+
+    console.table(user);
+
+    let userJSON = JSON.stringify(user);
+
+    sessionStorage.setItem(email, userJSON);
+}
